3f226ce9625682c32f4217d86501f78a48fc7f32
Thaís A <thais.antero@gmail.com>
2023-01-02 17:05:27 -0300

    Cria classe para inserir dados no banco a partir do path do csv

---
+++ lab01/app/csv_importer.rb
@@ -0,0 +1,25 @@
+require "csv"
+require "json"
+require "byebug"
+require_relative "./database"
+
+class CsvImporter
+  def self.old_import(csv_path)
+    rows = CSV.read(csv_path, col_sep: ';')
+
+    columns = rows.shift
+  
+    rows.map do |row|
+      row.each_with_object({}).with_index do |(cell, acc), idx|
+        column = columns[idx]
+        acc[column] = cell
+      end
+    end.to_json
+  end
+
+  def self.import(csv_path, db)
+    CSV.foreach(csv_path, col_sep: ';', headers: true) do |row|
+      db.insert_exam(row)
+    end
+  end
+end
