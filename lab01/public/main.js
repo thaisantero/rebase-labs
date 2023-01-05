a083ec92fde9d64e328bf25dd12d1f151d8a439c
Thaís A <thais.antero@gmail.com>
2023-01-05 17:45:41 -0300

    Implementa tabela e bootstrap para melhor visualizar os dados dos exames

--- lab01/public/main.js
+++ lab01/public/main.js
@@ -1,38 +1,48 @@
-const fragment = new DocumentFragment();
-const url = 'http://localhost:3000/tests'
+function tableFromJson() {
+  const url = 'http://localhost:3000/tests'
+  // the json data.
+  fetch(url, {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    },
+  })
+  .then(response => response.json())
+  .then(json => {
+    let col = ["cpf", "nome_paciente", "nome_médico", "tipo_exame", "resultado_tipo_exame"];
+    let col_names = ["CPF", "Nome do Paciente", "Médico", "Tipo de Exame", "Resultado do Exame"];
 
-fetch(url).
-  then((response) => response.json()).
-  then((data) => {
-    data.forEach(function(exams) {
-      const li = document.createElement('li');
-      li.textContent = `
-        ${exams.cpf} - 
-        ${exams.nome_paciente} -
-        ${exams.nome_médico} -
-        ${exams.tipo_exame} -
-        ${exams.limites_tipo_exame} -
-        ${exams.resultado_tipo_exame}
-      `;
-      fragment.appendChild(li);
-    })
-  }).
-  then(() => {
-    document.querySelector('ul').appendChild(fragment);
-  }).
-  catch(function(error) {
-    console.log(error);
-  });
+    // Create a table.
+    const table = document.createElement("table");
+    table.className = 'table table-striped table-hover'
 
-// fetch(url)
-//   .then(function(response){
-//     return response.json();
-//   })
-//   .then(function(data){
-//     let html = '';
-//     data.forEach(function(exam){
-//       html += `
-//         <li>${exams.nome_paciente}</li>`;
-//     });
-//     document.getElementById('cpf').innerHTML = html;
-//   })
+
+    // Create table header row using the extracted headers above.
+    let tr = table.insertRow(-1);                   // table row.
+
+    for (let i = 0; i < col_names.length; i++) {
+      let th = document.createElement("th");      // table header.
+      th.innerHTML = col_names[i];
+      tr.appendChild(th);
+    }
+
+    // add json data to the table as rows.
+    for (let i = 0; i < json.length; i++) {
+
+      tr = table.insertRow(-1);
+
+      for (let j = 0; j < col.length; j++) {
+        let tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = json[i][col[j]];
+      }
+    }
+
+    // Now, add the newly created table with json data, to a container.
+    const divShowData = document.getElementById('showData');
+    divShowData.innerHTML = "";
+    divShowData.appendChild(table);
+
+    console.log(json)
+  }
+  )
+}
